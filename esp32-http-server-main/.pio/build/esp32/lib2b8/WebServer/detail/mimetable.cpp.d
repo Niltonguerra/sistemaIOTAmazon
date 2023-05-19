93f4d75a1015684f693931b51398e11c4412a256
Nilton Dionisio Guerra <niltondg.39@gmail.com>
2023-05-18 21:58:44 -0300

    second commit

---
+++ esp32-http-server-main/.pio/build/esp32/lib2b8/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32/lib2b8/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/nilto/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/nilto/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/nilto/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
